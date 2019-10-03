3289169512254d8268292a2e307ed6f8015ac0fb
cuchanhonly <cuchanhonly@gmail.com>
2019-10-03 15:28:07 +0700

    4.Thêm lớp động vật chó mèo , Chó kêu gâu gâu , mèo kêu meo meo

---
+++ WpfApp1/DongVat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class DongVat
+    {
+            private string Keu;
+
+            public string DongvatKeu
+            {
+                get { return Keu; }
+                set { Keu = value; }
+            }
+
+            public DongVat(string keu)
+            {
+                this.Keu = keu;
+            }
+
+            public DongVat()
+            {
+            }
+            private string maulong;
+            public string MauLong
+            {
+                get { return maulong; }
+                set { maulong = value; }
+            }
+
+            public virtual void Talk()
+            {
+                if (DongvatKeu.Equals("Meo meo") && (MauLong.Equals("Trắng")))
+                    MessageBox.Show(this.DongvatKeu + "Tôi là Mèo lông màu" + this.MauLong);
+                else if (DongvatKeu.Equals("Gâu gâu") && (MauLong.Equals("Vàng")))
+                    MessageBox.Show(this.DongvatKeu + "Tôi là Chó lông màu" + this.MauLong);
+            }
+        }
+}
---
+++ WpfApp1/meo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Meo : DongVat
+    {
+        class Meo : DongVat
+        {
+            public override void Talk()
+            {
+                base.Talk();
+            }
+        }
+    }
+
+
+
+
